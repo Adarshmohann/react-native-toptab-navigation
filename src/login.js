f24a7079352f682bcfbe51fc3261bf3746482d05
adarshmohanann <adarshmonu2001@gmail.com>
2022-11-01 11:19:13 +0530

    first commit

--- src/login.js
+++ src/login.js
@@ -49,7 +49,7 @@ export default class Login extends Component{
            </TextInput>
            <TouchableHighlight style={styles.buttonView} 
            underlayColor='transparent'
-           onPress={()=> this.props.navigation.navigate('Flexbox',{Username:this.state.name})}>
+           onPress={()=> this.props.navigation.navigate('Tab',{Username:this.state.name})}>
             <Text style={styles.buttontext}>login
 
             </Text>
